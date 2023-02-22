ad667c5f8c8619a7da1a6cfdb767295ca6042812
Hannah Beattie <hannahdbeattie@gmail.com>
2023-02-22 22:38:05 +1300

    add new spattery timeline

--- features/layout/Timeline.tsx
+++ features/layout/Timeline.tsx
@@ -1,11 +1,27 @@
-import { FlatList, Heading, HStack, ScrollView, Tag, Text, VStack } from 'native-base'
+import {
+	Box,
+	FlatList,
+	Heading,
+	HStack,
+	Image,
+	ScrollView,
+	SectionList,
+	SimpleGrid,
+	Tag,
+	Text,
+	View,
+	VStack,
+} from 'native-base'
 import React from 'react'
-import { useWindowDimensions } from 'react-native'
+import { ImageBackground, useWindowDimensions } from 'react-native'
 
 import { useSelector } from 'react-redux'
 import { useFormatDate } from '~features/worries/useFormatDate'
 import { selectAllInactive } from '~features/worries/worrySlice'
 
+const spatter = require('../../assets/spatter01.png')
+const spatter2 = require('../../assets/spatter02.png')
+const spatter3 = require('../../assets/spatter03.png')
 function Timeline() {
 	const { height, width } = useWindowDimensions()
 	let worryData = useSelector(selectAllInactive).reverse()
@@ -17,47 +33,121 @@ function Timeline() {
 		}
 	})
 	let dates = entries.forEach((e) => e.time)
+
 	return (
-		<ScrollView>
-			<VStack variant={'page'} height={height}>
-				<Heading>Blue's food diary</Heading>
-				<FlatList
-					py={100}
-					horizontal={true}
-					data={entries}
-					renderItem={({ item }) => (
-						<HStack>
-							<Tag backgroundColor={'amber.200'}>{item.time}</Tag>
-							<VStack py={2}>
-								<Tag>{item.description}</Tag>
+		<ScrollView horizontal={true} backgroundColor={'gray.900'}>
+			<VStack>
+				<HStack space={8}>
+					<VStack maxH={width / 2}>
+						<Image
+							overflow={'visible'}
+							p={4}
+							boxSize={'600'}
+							resizeMode={'contain'}
+							alt='spatter'
+							source={spatter3}
+						/>
+					</VStack>
+					<VStack maxH={width / 2}>
+						<Image
+							overflow={'visible'}
+							p={4}
+							boxSize={'600'}
+							resizeMode={'contain'}
+							alt='spatter'
+							source={spatter2}
+						/>
+					</VStack>
+
+					<VStack maxH={width / 2}>
+						<Image
+							overflow={'visible'}
+							p={4}
+							boxSize={'600'}
+							resizeMode={'contain'}
+							alt='spatter'
+							source={spatter2}
+						/>
+					</VStack>
+					<VStack maxH={width / 2}>
+						<Image
+							overflow={'visible'}
+							p={4}
+							boxSize={'600'}
+							resizeMode={'contain'}
+							alt='spatter'
+							source={spatter2}
+						/>
+					</VStack>
+				</HStack>
+				<VStack>
+					<Heading px={8}>Blue's food diary</Heading>
+					<FlatList
+						px={4}
+						maxWidth={'container'}
+						showsHorizontalScrollIndicator={false}
+						pt={100}
+						horizontal={true}
+						data={entries}
+						renderItem={({ item }) => (
+							<VStack key={item.time} marginRight={4} space={0}>
+								<Box>
+									<Tag>{item.time}</Tag>
+								</Box>
+								<VStack py={4} backgroundColor={'blue.900'} space={2}>
+									<Tag backgroundColor={'red.200'}>{item.title}</Tag>
+									<Tag>{item.description}</Tag>
+								</VStack>
 							</VStack>
-						</HStack>
-					)}
-					keyExtractor={(item) => item.time}
-				></FlatList>
+						)}
+					/>
+
+					<HStack space={8}>
+						<VStack maxH={width / 2}>
+							<Image
+								overflow={'visible'}
+								p={4}
+								boxSize={'600'}
+								resizeMode={'contain'}
+								alt='spatter'
+								source={spatter2}
+							/>
+						</VStack>
+						<VStack maxH={width / 2}>
+							<Image
+								overflow={'visible'}
+								p={4}
+								boxSize={'600'}
+								resizeMode={'contain'}
+								alt='spatter'
+								source={spatter2}
+							/>
+						</VStack>
+						<VStack maxH={width / 2}>
+							<Image
+								overflow={'visible'}
+								p={4}
+								boxSize={'600'}
+								resizeMode={'contain'}
+								alt='spatter'
+								source={spatter3}
+							/>
+						</VStack>
+						<VStack maxH={width / 2}>
+							<Image
+								overflow={'visible'}
+								p={4}
+								boxSize={'600'}
+								resizeMode={'contain'}
+								alt='spatter'
+								source={spatter2}
+							/>
+						</VStack>
+					</HStack>
+				</VStack>
 			</VStack>
 		</ScrollView>
 	)
 }
 
 export default Timeline
-
-// <ScrollView>
-// 			<VStack variant={'page'} height={height}>
-// 				<Heading>Blue's food diary</Heading>
-// 				<FlatList
-// 					py={100}
-// 					horizontal={true}
-// 					data={entries}
-// 					renderItem={({ item }) => (
-// 						<HStack>
-// 							<Tag backgroundColor={'amber.200'}>{item.time}</Tag>
-// 							<VStack py={2}>
-// 								<Tag>{item.description}</Tag>
-// 							</VStack>
-// 						</HStack>
-// 					)}
-// 					keyExtractor={(item) => item.time}
-// 				></FlatList>
-// 			</VStack>
-// 		</ScrollView>
