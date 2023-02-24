4d1e0e03bbd7e4809cc15424ef5870c6a2598596
Hannah Beattie <hannahdbeattie@gmail.com>
2023-02-24 14:48:25 +1300

    make form reusable with props

--- app/addWorry.tsx
+++ app/addWorry.tsx
@@ -1,42 +1,71 @@
 // import { checkTargetForNewValues } from 'framer-motion'
 import { Feather } from '@expo/vector-icons'
-import { Divider, Heading, HStack, Input, KeyboardAvoidingView, Spacer, VStack } from 'native-base'
+import { useRouter } from 'expo-router'
+import {
+	Button,
+	Divider,
+	Heading,
+	HStack,
+	Input,
+	KeyboardAvoidingView,
+	Spacer,
+	Text,
+	VStack,
+} from 'native-base'
 import React, { useCallback, useEffect, useRef } from 'react'
 import { StyleSheet, TextInput, useWindowDimensions } from 'react-native'
 import { TouchableOpacity } from 'react-native-gesture-handler'
 // import HomeButton from '~features/layout/HomeButton'
 
-export default function AddWorry() {
-	// const textareaRef = useRef<typeof TextArea>(null)
-	const textareaRef = useRef<TextInput>(null)
+interface FormProps {
+	question: string
+	placeholder: string
+	icon: any
+	buttonText: string
+	hideButton: boolean
+	nextRoute: string
+	handleButton: any
+}
+
+export default function AddWorry({
+	question,
+	placeholder,
+	icon,
+	buttonText,
+	hideButton,
+	nextRoute,
+	handleButton,
+}: FormProps) {
+	// const textareaRef = useRef<TextInput>(null)
 
 	const handleChange = useCallback((text: string) => {
 		console.log('Changed value to: ', text)
 	}, [])
 
-	useEffect(() => {
-		if (textareaRef.current === null) {
-			return
-		}
-		// ;(window as any).ta = textareaRef.current
-		textareaRef.current.focus()
-	}, [textareaRef])
+	// useEffect(() => {
+	// 	if (textareaRef.current === null) {
+	// 		return
+	// 	}
+
+	// 	textareaRef.current.focus()
+	// }, [textareaRef])
 
 	const { height, width } = useWindowDimensions()
+	const router = useRouter()
 	return (
 		<KeyboardAvoidingView behavior='padding' style={styles.form}>
-			<VStack space={4}>
+			<VStack space={4} h={height}>
 				<Heading fontFamily='Poppins_300Light' color={'black'}>
-					I am feeling worried because...
+					{question ? question : 'question'}
 				</Heading>
 
 				<Input
 					onChangeText={handleChange}
-					ref={textareaRef}
+					// ref={textareaRef}
 					multiline
 					color={'blueGray.900'}
 					placeholderTextColor={'blueGray.500'}
-					placeholder='I am worried...'
+					placeholder={placeholder ? placeholder : 'placeholder'}
 					size='2xl'
 					fontSize='2xl'
 					fontFamily='Poppins_300Light'
@@ -47,9 +76,29 @@ export default function AddWorry() {
 				/>
 				<Divider />
 				<HStack>
+					{!hideButton && (
+						<TouchableOpacity>
+							<Button
+								onPress={() => {
+									handleButton()
+								}}
+							>
+								<Text>{buttonText ? buttonText : 'button'}</Text>
+							</Button>
+						</TouchableOpacity>
+					)}
 					<Spacer />
-					<TouchableOpacity area-accessibilityLabel='navagate next'>
-						<Feather name='arrow-right-circle' size={30} color='gray.900' />
+					<TouchableOpacity
+						area-accessibilityLabel='navagate next'
+						onPress={() => {
+							router.push(nextRoute ? nextRoute : '/monsterMenu')
+						}}
+					>
+						{icon ? (
+							icon
+						) : (
+							<Feather name='arrow-right-circle' size={30} color='gray.900' />
+						)}
 					</TouchableOpacity>
 				</HStack>
 			</VStack>
