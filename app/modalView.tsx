64fb8aba12d1279906b6b4715b6880f557c0ec61
Hannah Beattie <hannahdbeattie@gmail.com>
2023-02-28 02:45:46 +1300

    add warn

--- app/modalView.tsx
+++ app/modalView.tsx
@@ -1,84 +1,8 @@
-import { Center, Spacer, VStack } from 'native-base'
-import React, { FC, useState, ReactElement } from 'react'
-import { Text, TouchableOpacity, StyleSheet, View, useWindowDimensions } from 'react-native'
-import Modal from 'react-native-modal'
+import React from 'react'
+import MyModal from '~features/layout/MyModal'
 
-interface Props {
-	fullWorry: ReactElement
-	partialWorry: ReactElement
+function modalView() {
+	return <MyModal />
 }
 
-const ModalView: FC<Props> = ({ fullWorry, partialWorry }) => {
-	const { height, width } = useWindowDimensions()
-	const [visibleModal, setVisibleModal] = useState<number | null>(null)
-
-	const renderButton = (text: string, onPress: () => void) => (
-		<TouchableOpacity onPress={onPress}>
-			{partialWorry}
-			<View style={styles.button}>
-				<Text>{text}</Text>
-			</View>
-		</TouchableOpacity>
-	)
-
-	const renderModalContent = () => (
-		<Center
-			flex={1}
-			width={width}
-			height={height}
-			alignItems={'stretch'}
-			backgroundColor={'gray.900'}
-			p={0}
-		>
-			<VStack flex={1}>{fullWorry}</VStack>
-			{renderButton('Close', () => setVisibleModal(null))}
-		</Center>
-	)
-
-	return (
-		<View style={styles.container}>
-			{renderButton('Full Screen modal', () => setVisibleModal(1))}
-
-			<Modal
-				isVisible={visibleModal === 1}
-				backdropColor={''}
-				animationIn={'zoomInDown'}
-				animationOut={'zoomOutUp'}
-				animationInTiming={1000}
-				animationOutTiming={1000}
-				style={{ margin: 0 }}
-			>
-				{renderModalContent()}
-			</Modal>
-		</View>
-	)
-}
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		justifyContent: 'center',
-		alignItems: 'center',
-	},
-	button: {
-		backgroundColor: 'lightblue',
-		padding: 12,
-		margin: 16,
-		justifyContent: 'center',
-		alignItems: 'center',
-		borderRadius: 4,
-	},
-	modalContent: {
-		backgroundColor: 'white',
-		padding: 22,
-		justifyContent: 'center',
-		alignItems: 'center',
-		borderRadius: 4,
-	},
-	bottomModal: {
-		justifyContent: 'flex-end',
-		margin: 0,
-	},
-})
-
-export default ModalView
+export default modalView
---
+++ features/worries/Warn.tsx
@@ -0,0 +1,94 @@
+import { AntDesign } from '@expo/vector-icons'
+import {
+	Center,
+	VStack,
+	HStack,
+	IconButton,
+	CloseIcon,
+	Box,
+	Text,
+	Alert,
+	Button,
+	Collapse,
+	Heading,
+	Spacer,
+} from 'native-base'
+import React, { Children } from 'react'
+import { Pressable, useWindowDimensions } from 'react-native'
+
+export default function Warn({ children }: any) {
+	const [show, setShow] = React.useState(false)
+	const { height, width } = useWindowDimensions()
+	return (
+		<VStack w='100%' alignItems='center'>
+			<Collapse
+				isOpen={show}
+				width={width}
+				justifyContent={'center'}
+				justifyItems={'center'}
+				alignContent={'center'}
+				alignSelf={'center'}
+				marginTop={3}
+			>
+				<Alert status='error' py={2} my={2}>
+					<VStack space={1} flexShrink={1} w='100%'>
+						<HStack
+							flexShrink={1}
+							space={2}
+							alignItems='center'
+							justifyContent='space-between'
+						>
+							<HStack flexShrink={1} space={2} alignItems='center'>
+								<Alert.Icon />
+								<Text
+									fontSize='md'
+									fontWeight='medium'
+									_dark={{
+										color: 'coolGray.800',
+									}}
+								>
+									Are you sure?
+								</Text>
+							</HStack>
+
+							<IconButton
+								variant='unstyled'
+								_focus={{
+									borderWidth: 0,
+								}}
+								icon={<CloseIcon size='3' />}
+								_icon={{
+									color: 'coolGray.600',
+								}}
+								onPress={() => setShow(false)}
+							/>
+						</HStack>
+						<Center>
+							<HStack space={1}>
+								<Button variant={'ghost'} backgroundColor={'red.800'}>
+									<Heading color={'red.100'}>Yip!</Heading>
+								</Button>
+								<Button variant={'ghost'} backgroundColor={'gray.800'}>
+									<Heading color={'white'}>Oops!</Heading>
+								</Button>
+							</HStack>
+						</Center>
+					</VStack>
+				</Alert>
+			</Collapse>
+			<HStack pt={4}>
+				<Button
+					backgroundColor='gray.800'
+					py={3}
+					px={3}
+					borderRadius={200}
+					_pressed={{ backgroundColor: 'red.800' }}
+					onPress={() => setShow(true)}
+				>
+					<AntDesign name='delete' size={24} color='white' />
+				</Button>
+				<Spacer />
+			</HStack>
+		</VStack>
+	)
+}
