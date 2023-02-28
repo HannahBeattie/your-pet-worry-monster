2588d1a90f6aa52bbb5aae1ad7504b84accbe0b3
Mikey Lemmon <mikeylemmon@gmail.com>
2023-02-28 22:56:18 +1300

    Make DragExpander nice and centered in the screen

--- features/experiments/DragExpander.tsx
+++ features/experiments/DragExpander.tsx
@@ -1,49 +1,27 @@
 import { Box, VStack } from 'native-base'
-import { InterfaceVStackProps } from 'native-base/lib/typescript/components/primitives/Stack/VStack'
-import { ComponentProps, FC, ReactElement, useRef, useState } from 'react'
-import { View, useWindowDimensions } from 'react-native'
+import { ComponentProps, FC, ReactElement } from 'react'
 import { Gesture, GestureDetector } from 'react-native-gesture-handler'
 import Animated, {
 	Extrapolation,
-	FadeIn,
-	FadeInUp,
-	FadeOut,
-	FadeOutDown,
-	FadingTransition,
-	Layout,
-	StyleProps,
-	Transition,
 	interpolate,
-	measure,
-	runOnJS,
 	runOnUI,
 	useAnimatedRef,
 	useAnimatedStyle,
 	useDerivedValue,
 	useSharedValue,
 	withSpring,
-	withTiming,
 } from 'react-native-reanimated'
 
-const AVStack = Animated.createAnimatedComponent(VStack)
-
 export type DragExpanderProps = ComponentProps<typeof VStack> & {
-	expanded?: ReactElement
-	initialHeight?: number
-	_animated?: StyleProps
+	header: ReactElement
 	_bg?: ComponentProps<typeof Box>
 }
 
-const snaps = {
-	down: 250, // y location when not expanded
-	escape: 20, // how far you have to drag before it flips to the other snap point
-}
+const snapEscape = 20 // distance that the user needs to drag to snap to the other position
 
 const DragExpander: FC<DragExpanderProps> = ({
-	expanded,
-	_animated,
+	header,
 	_bg,
-	initialHeight,
 	p,
 	px,
 	py,
@@ -55,50 +33,51 @@ const DragExpander: FC<DragExpanderProps> = ({
 	...rest
 }) => {
 	const padProps = { p, px, py, pl, pt, pr, pb }
-	const refAlways = useAnimatedRef()
+	const refHeader = useAnimatedRef()
 	const refExpanded = useAnimatedRef()
 
 	const isUp = useSharedValue(false) // start out not-expanded
 	const isPressed = useSharedValue(false) // we're actually not using this right now, but you could use it to adjust scale/etc based on if the user is currently pressing
-	const alwaysH = useSharedValue(50) // this is used to store the height for the component when it isn't expanded
+	const expanded = useSharedValue(0) // 0-to-1 animated value for whether we're expanded or not
+	const expandedSpring = useDerivedValue(() =>
+		withSpring(expanded.value, { stiffness: 250, damping: 21 })
+	) // springy value for expanded
+	const headerH = useSharedValue(50) // this is used to store the height for the component when it isn't expanded
 	const expandedH = useSharedValue(100) // this is used to store the height for the expanded part of the component
-	const offY = useSharedValue(snaps.down) // y-location ("offset") for the components that are being animated
-	const startY = useSharedValue(snaps.down) // y-location when the user started the pan gesture, used for internal computations in the gesture handlers
-	const snapsUp = useDerivedValue(() => snaps.down - expandedH.value / 2)
+	const snapDown = useDerivedValue(() => expandedH.value / 2) // y offset when not expanded
+	const snapUp = useDerivedValue(() => 0) // y offset when expanded
+	const offY = useSharedValue(snapDown.value) // y-location ("offset") for the components that are being animated
+	const startY = useSharedValue(snapDown.value) // y-location when the user started the pan gesture, used for internal computations in the gesture handlers
+
+	// animStyle moves the whole component
 	const animStyle = useAnimatedStyle(() => {
 		return {
 			transform: [{ translateY: offY.value }],
 		}
 	})
 
+	// animStyleBg controls the height of the background
 	const animStyleBg = useAnimatedStyle(() => {
 		return {
 			height: withSpring(
 				interpolate(
-					offY.value,
-					[snaps.down - snaps.escape, snapsUp.value + snaps.escape],
-					[alwaysH.value, alwaysH.value + expandedH.value],
+					expanded.value,
+					[0.2, 0.7],
+					[headerH.value, headerH.value + expandedH.value],
 					Extrapolation.CLAMP
-				)
+				),
+				{ stiffness: 300, damping: 20 }
 			),
 		}
 	})
 
+	// animStyleExpanded controls the styling of the content that is only visible when expanded
 	const animStyleExpanded = useAnimatedStyle(() => {
-		const snapsHalf = (snaps.down - snapsUp.value) / 2 + snapsUp.value
-		const visibility = interpolate(
-			offY.value,
-			[snapsHalf, snapsUp.value],
-			[0, 1],
-			Extrapolation.CLAMP
-		)
+		const visibility = interpolate(expandedSpring.value, [0.75, 1], [0, 1])
+		const scale = interpolate(expandedSpring.value, [0.75, 1], [0.5, 1])
 		return {
 			opacity: visibility,
-			transform: [
-				{
-					scale: withSpring(visibility * 0.5 + 0.5),
-				},
-			],
+			transform: [{ scale }],
 		}
 	})
 
@@ -106,22 +85,30 @@ const DragExpander: FC<DragExpanderProps> = ({
 		.activeOffsetY([-5, 5])
 		.onBegin(() => {
 			isPressed.value = true
-			expandedH.value = measure(refExpanded).height
 		})
 		.onUpdate((e) => {
 			// update y offset based on how far the user has dragged since the start of the gesture
 			offY.value = e.translationY + startY.value
+			// update our 0-to-1 "expanded" animated value based on where we are between the snap points
+			expanded.value = interpolate(
+				offY.value,
+				[snapDown.value, snapUp.value],
+				[0, 1],
+				Extrapolation.CLAMP
+			)
 		})
 		.onEnd(() => {
 			// Now that the gesture has finished, we're going to snap to either the 'down' or the 'up' snap point
-			// First, figure out how far we've dragged away from the current snap point
-			const diff = isUp.value ? offY.value - snapsUp.value : snaps.down - offY.value
-			if (diff > snaps.escape) {
+			// First, figure out how far we've dragged away from the current snap point, and toggle isUp if we've got far enough
+			const diff = isUp.value ? offY.value - snapUp.value : snapDown.value - offY.value
+			if (diff > snapEscape) {
 				// We've dragged enough to escape the current snap, toggle isUp
 				isUp.value = !isUp.value
 			}
 			// Choose our snap target based on the updated value of isUp
-			const snapTo = isUp.value ? snapsUp.value : snaps.down
+			const snapTo = isUp.value ? snapUp.value : snapDown.value
+			// Update our 0-to-1 "expanded" value
+			expanded.value = isUp.value ? 1 : 0
 			// Spring to our snap target!
 			offY.value = withSpring(snapTo)
 			startY.value = withSpring(snapTo)
@@ -131,56 +118,69 @@ const DragExpander: FC<DragExpanderProps> = ({
 		})
 
 	return (
-		<VStack alignItems='stretch' flex={1}>
-			<GestureDetector gesture={gesture}>
-				<Animated.View
-					style={[animStyle, _animated, { display: 'flex', alignItems: 'stretch' }]}
+		<GestureDetector gesture={gesture}>
+			<Animated.View style={[animStyle, { display: 'flex', alignItems: 'stretch' }]}>
+				<VStack
+					alignItems='stretch'
+					justifyContent='center'
+					w='100%'
+					position='relative'
+					{...rest}
 				>
-					<VStack
-						alignItems='stretch'
-						justifyContent='center'
-						w='100%'
-						position='relative'
-						{...rest}
+					<Box
+						// This is a view that renders the background behind the other components
+						{..._bg}
+						{...padProps}
+						position='absolute'
+						left={0}
+						top={0}
+						right={0}
 					>
-						<Box
-							// This is a view that renders the background behind the other components
-							{..._bg}
-							{...padProps}
-							position='absolute'
-							left={0}
-							top={0}
-							right={0}
+						<Animated.View style={[animStyleBg]} />
+					</Box>
+					<VStack alignItems='stretch' {...padProps}>
+						<VStack
+							// This view contains the "header" and is always displayed
+							alignItems='stretch'
+							ref={refHeader}
+							onLayout={({ nativeEvent }) => {
+								// Set the "headerH" value now that we know how big it is
+								runOnUI((height) => {
+									'worklet'
+									if (height > 0) {
+										headerH.value = height
+									}
+								})(nativeEvent.layout.height)
+							}}
 						>
-							<Animated.View style={[animStyleBg]} />
-						</Box>
-						<VStack alignItems='stretch' {...padProps}>
-							<VStack
-								alignItems='stretch'
-								ref={refAlways}
-								onLayout={({ nativeEvent }) => {
-									runOnUI((height) => {
-										'worklet'
-										if (height > 0) {
-											alwaysH.value = height
-										}
-									})(nativeEvent.layout.height)
-								}}
+							{header}
+						</VStack>
+						<VStack
+							alignItems='stretch'
+							ref={refExpanded}
+							onLayout={({ nativeEvent }) => {
+								// Set the "expandedH" value now that we know how big it is
+								// and update "offY" to center the component
+								runOnUI((height) => {
+									'worklet'
+									if (height > 0) {
+										expandedH.value = height
+										offY.value = height / 2
+									}
+								})(nativeEvent.layout.height)
+							}}
+						>
+							<Animated.View
+								style={[animStyleExpanded, { display: 'flex', width: '100%' }]}
+								// this view contains content that is only displayed when expanded
 							>
 								{children}
-							</VStack>
-							<VStack alignItems='stretch' ref={refExpanded}>
-								<Animated.View
-									style={[animStyleExpanded, { display: 'flex', width: '100%' }]}
-								>
-									{expanded}
-								</Animated.View>
-							</VStack>
+							</Animated.View>
 						</VStack>
 					</VStack>
-				</Animated.View>
-			</GestureDetector>
-		</VStack>
+				</VStack>
+			</Animated.View>
+		</GestureDetector>
 	)
 }
 export default DragExpander
--- features/experiments/WorryFeature.tsx
+++ features/experiments/WorryFeature.tsx
@@ -1,63 +1,52 @@
-import { Heading, HStack, ScrollView, Text, VStack } from 'native-base'
+import { HStack, ScrollView, Text, VStack } from 'native-base'
 import React from 'react'
-import { useSelector } from 'react-redux'
-import { useFormatDate } from '~features/worries/useFormatDate'
-import { selectAllInactive, Worry } from '~features/worries/worrySlice'
-import DragExpander from './DragExpander'
 import { useWindowDimensions } from 'react-native'
+import { useSelector } from 'react-redux'
 import { monsterNameSelector } from '~features/monster/monsterSlice'
-
-const longWorry: Worry = {
-	id: +new Date(),
-	description: `Descriptive stuff all goes here! Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.`,
-	extraNote: `Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.`,
-	sensation: `labore et dolore magna aliqua. Ut enim ad minim`,
-	consumedAt: +new Date() - 400000,
-	isActive: false,
-}
+import { useFormatDate } from '~features/worries/useFormatDate'
+import { selectAllInactive } from '~features/worries/worrySlice'
+import DragExpander from './DragExpander'
 
 function WorryFeature() {
 	const worryData = useSelector(selectAllInactive)
 	const monsterName = useSelector(monsterNameSelector)
 	const { width } = useWindowDimensions()
-	const bg = '#444'
 	return (
 		<VStack variant={'page'}>
-			<ScrollView horizontal={true} scrollEnabled>
-				<HStack flex={1} space={4}>
+			<ScrollView horizontal={true} scrollEnabled flex={1}>
+				<HStack flex={1} space={4} alignItems='center'>
 					{worryData.map((worry) => (
 						<DragExpander
 							key={worry.id}
 							_bg={{ bg: 'gray.700', borderRadius: 'lg' }}
 							p={8}
 							maxW={width * 0.7}
-							expanded={
-								<VStack space={4} pt={4} alignItems='stretch'>
-									{worry.sensation && (
-										<Text fontSize={'sm'}>I felt it in {worry.sensation}</Text>
-									)}
-									{worry.extraNote && (
-										<Text fontSize={'sm'}>
-											The scariest part was {worry.extraNote}
-										</Text>
-									)}
-									<VStack alignItems='flex-end'>
-										<Text fontSize={'xs'} fontStyle='italic'>
-											Worried {useFormatDate(worry.id)}
-										</Text>
-										{worry.consumedAt && (
-											<Text fontSize={'xs'} fontStyle='italic'>
-												Eaten by {monsterName}{' '}
-												{useFormatDate(worry.consumedAt)}
-											</Text>
-										)}
-									</VStack>
-								</VStack>
+							header={
+								<Text fontSize={'sm'} width='100%'>
+									I worried that {worry.description}
+								</Text>
 							}
 						>
-							<Text fontSize={'sm'} width='100%'>
-								I worried that {worry.description}
-							</Text>
+							<VStack space={4} pt={4} alignItems='stretch'>
+								{worry.sensation && (
+									<Text fontSize={'sm'}>I felt it in {worry.sensation}</Text>
+								)}
+								{worry.extraNote && (
+									<Text fontSize={'sm'}>
+										The scariest part was {worry.extraNote}
+									</Text>
+								)}
+								<VStack alignItems='flex-end'>
+									<Text fontSize={'xs'} fontStyle='italic'>
+										Worried {useFormatDate(worry.id)}
+									</Text>
+									{worry.consumedAt && (
+										<Text fontSize={'xs'} fontStyle='italic'>
+											Eaten by {monsterName} {useFormatDate(worry.consumedAt)}
+										</Text>
+									)}
+								</VStack>
+							</VStack>
 						</DragExpander>
 					))}
 				</HStack>
