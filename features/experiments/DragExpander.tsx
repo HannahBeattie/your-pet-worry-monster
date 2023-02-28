522bedc8efb6c1e41b62c664d4032ef91e2e3f3c
Mikey Lemmon <mikeylemmon@gmail.com>
2023-02-28 14:24:48 +1300

    Add some helpful comments to DragExpander

--- features/experiments/DragExpander.tsx
+++ features/experiments/DragExpander.tsx
@@ -15,17 +15,17 @@ export type DragExpanderProps = PropsWithChildren<{
 
 const DragExpander: FC<DragExpanderProps> = ({ children, expanded }) => {
 	const snaps = {
-		up: 80,
-		down: 250,
+		down: 250, // y location when not expanded
+		up: 80, // y location when expanded
 		half: 0, // halfway between up and down, computed below
-		escape: 20,
+		escape: 20, // how far you have to drag before it flips to the other snap point
 	}
 	snaps.half = (snaps.down - snaps.up) / 2 + snaps.up
 
-	const isPressed = useSharedValue(false)
-	const isUp = useSharedValue(false)
-	const offY = useSharedValue(snaps.down)
-	const startY = useSharedValue(snaps.down)
+	const isUp = useSharedValue(false) // start out not-expanded
+	const isPressed = useSharedValue(false) // we're actually not using this right now, but you could use it to adjust scale/etc based on if the user is currently pressing
+	const offY = useSharedValue(snaps.down) // y-location ("offset") for the components that are being animated
+	const startY = useSharedValue(snaps.down) // y-location when the user started the pan gesture, used for internal computations in the gesture handlers
 	const style = useAnimatedStyle(() => ({
 		transform: [{ translateY: offY.value }],
 		height: interpolate(
@@ -43,15 +43,20 @@ const DragExpander: FC<DragExpanderProps> = ({ children, expanded }) => {
 			isPressed.value = true
 		})
 		.onUpdate((e) => {
+			// update y offset based on how far the user has dragged since the start of the gesture
 			offY.value = e.translationY + startY.value
 		})
 		.onEnd(() => {
+			// Now that the gesture has finished, we're going to snap to either the 'down' or the 'up' snap point
+			// First, figure out how far we've dragged away from the current snap point
 			const diff = isUp.value ? offY.value - snaps.up : snaps.down - offY.value
 			if (diff > snaps.escape) {
-				// We've dragged enough to escape the current snap, toggle isUp!
+				// We've dragged enough to escape the current snap, toggle isUp
 				isUp.value = !isUp.value
 			}
+			// Choose our snap target based on the updated value of isUp
 			const snapTo = isUp.value ? snaps.up : snaps.down
+			// Spring to our snap target!
 			offY.value = withSpring(snapTo)
 			startY.value = withSpring(snapTo)
 		})
