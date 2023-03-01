e869e9c61bd45226b345f719112adc97d096c95d
Hannah Beattie <hannahdbeattie@gmail.com>
2023-03-01 20:28:38 +1300

    change worry input to dark theme, style worry options

--- features/worries/WorryOptions.tsx
+++ features/worries/WorryOptions.tsx
@@ -1,28 +1,20 @@
-import { Tab } from '@chakra-ui/react'
+import { MaterialCommunityIcons } from '@expo/vector-icons'
 import { useRouter } from 'expo-router'
-import { Button, Divider, Heading, HStack, Image, Spacer, Tag, Text, VStack } from 'native-base'
+import { Center, Heading, HStack, Icon, IconButton, Image, Spacer, Text, VStack } from 'native-base'
 import React from 'react'
-import { SafeAreaView, TouchableOpacity, useWindowDimensions } from 'react-native'
+import { SafeAreaView, useWindowDimensions } from 'react-native'
 import { useDispatch, useSelector } from 'react-redux'
 import DragExpander from '~features/experiments/DragExpander'
-import Blue from '~features/monster/Blue'
 import { useFormatDate } from './useFormatDate'
-import {
-	deleteWorry,
-	selectAllActive,
-	selectLastActiveItem,
-	updateWorry,
-	Worry,
-} from './worrySlice'
-const worryBags = require('../../assets/worrybags.png')
-const blueFace = require('../../assets/blueFace.png')
+import { deleteWorry, selectLastActiveItem, updateWorry } from './worrySlice'
+
 const hang = require('../../assets/hang.png')
 
 function WorryOptions() {
 	const router = useRouter()
 	const dispatch = useDispatch()
 	const latestActive = useSelector(selectLastActiveItem)
-	const { width, height } = useWindowDimensions()
+	const { width } = useWindowDimensions()
 
 	const textProps = {
 		fontFamily: 'Poppins_300Light',
@@ -52,45 +44,108 @@ function WorryOptions() {
 			</HStack>
 
 			<SafeAreaView style={{ flex: 1 }}>
-				<VStack flex={1} px={4} justifyContent={'space-evenly'}>
-					{latestActive && (
-						<DragExpander
-							onDelete={() => {
-								dispatch(deleteWorry(latestActive.id))
-								router.push('/')
-							}}
-							backgroundColor={'black'}
-							borderRadius={'lg'}
-							px={4}
-							py={2}
-							borderColor={'red.600'}
-							borderWidth={0.5}
-							header={
-								<VStack space={2}>
-									<Text
-										{...textProps}
-										fontFamily={'Poppins_500Medium'}
-										color={'red.300'}
-									>
-										{useFormatDate(latestActive?.id)}
-									</Text>
-									<Text {...textProps}>
-										I am worried {latestActive?.description}
-									</Text>
+				<VStack flex={1} px={4}>
+					<Spacer />
+					<Center>
+						{latestActive && (
+							<DragExpander
+								px={4}
+								backgroundColor={'gray.900'}
+								borderRadius={'md'}
+								onDelete={() => {
+									dispatch(deleteWorry(latestActive.id))
+									router.push('/')
+								}}
+								header={
+									<VStack>
+										<Text
+											{...textProps}
+											fontFamily={'Poppins_500Medium'}
+											color={'red.300'}
+										>
+											{useFormatDate(latestActive?.id)}
+										</Text>
+										<Text {...textProps}>
+											I am worried {latestActive?.description}
+										</Text>
+									</VStack>
+								}
+							>
+								<VStack>
+									<Text {...textProps}>{latestActive?.extraNote}</Text>
+									<Text {...textProps}>{latestActive?.sensation}</Text>
 								</VStack>
-							}
-						>
-							<VStack py={2}>
-								<Text {...textProps}>{latestActive?.extraNote}</Text>
-								<Text {...textProps}>{latestActive?.sensation}</Text>
-							</VStack>
-						</DragExpander>
-					)}
-
-					<VStack space={4}>
-						<Text fontFamily={'Poppins_500Medium'} fontSize={'md'} px={4}>
-							Wouly you like me to hold on to this worry for a bit?
+							</DragExpander>
+						)}
+					</Center>
+					<Spacer />
+					<VStack mx={-4} py={2} pb={4} bg={'black'} mb={4} mt={4}>
+						<Text {...textProps} px={10} fontSize={'xl'} color={'white'}>
+							Should I gobble this worry up
 						</Text>
+						<Text {...textProps} px={10} fontSize={'xl'} color={'white'}>
+							or save it for later ?
+						</Text>
+					</VStack>
+
+					<HStack justifyContent={'space-evenly'} px={50}>
+						<VStack>
+							<IconButton
+								onPress={() => {
+									dispatch(
+										updateWorry({
+											id: latestActive.id,
+											changes: {
+												isActive: !latestActive.isActive,
+												consumedAt: +new Date(),
+											},
+										})
+									),
+										router.push('/eatingWorry')
+								}}
+								icon={
+									<Icon
+										size={'5xl'}
+										as={MaterialCommunityIcons}
+										name={'food-drumstick'}
+									/>
+								}
+							/>
+							<VStack>
+								<Heading fontSize={'sm'} textAlign={'center'}>
+									Gobble
+								</Heading>
+							</VStack>
+						</VStack>
+
+						<VStack>
+							<IconButton
+								onPress={() => {
+									router.push('/')
+								}}
+								icon={
+									<Icon
+										size={'5xl'}
+										as={MaterialCommunityIcons}
+										name={'food-drumstick-off'}
+									/>
+								}
+							/>
+							<Heading fontSize={'sm'} textAlign={'center'}>
+								Save
+							</Heading>
+						</VStack>
+					</HStack>
+				</VStack>
+			</SafeAreaView>
+		</VStack>
+	)
+}
+
+export default WorryOptions
+
+{
+	/* 						
 						<Button
 							onPress={() => {
 								dispatch(
@@ -140,12 +195,5 @@ function WorryOptions() {
 							>
 								Yes Please.
 							</Text>
-						</Button>
-					</VStack>
-				</VStack>
-			</SafeAreaView>
-		</VStack>
-	)
+						</Button> */
 }
-
-export default WorryOptions
