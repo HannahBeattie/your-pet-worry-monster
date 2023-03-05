a0305d039c9b1f0b635292e928a8329a79f55cda
Hannah Beattie <hannahdbeattie@gmail.com>
2023-03-05 14:54:19 +1300

    make intro notes

---
+++ app/intro.tsx
@@ -0,0 +1,44 @@
+import { Heading } from 'native-base'
+import React from 'react'
+import PageWrapper from '~features/styledComponents/PageWrapper'
+
+// fade in from black, center screen
+//'GRRR!', 'ROAR', 'GROWLLLLL!', 'I am the dreaded....'
+
+//text appears at top of page
+//"WORRY MONSTER!!"
+
+// Gregory blue appears, smiling
+
+//text sequence continues above him :
+// "I like to eat worries!"
+// "Delicious!"
+// "My name is..."
+// "Wait!"
+
+// Gregory looks sad/confused
+
+// "I don't have a name!"
+// "I can't be a fearsome worry monster without a name!"
+
+//link to '/name' which is followed by '/confirm name'
+
+// text sequence continues above gregory:
+
+// "*GURGLE GURLGE*"
+// "I am hungry!"
+// "Do you have any worries for me to eat?"
+
+// buttons appear :
+// ADD WORRY ---- '/addWorry'  === intro played
+// MAYBE LATER --- '/' === intro played
+
+function intro() {
+	return (
+		<PageWrapper>
+			<Heading>intro goes here!</Heading>
+		</PageWrapper>
+	)
+}
+
+export default intro
