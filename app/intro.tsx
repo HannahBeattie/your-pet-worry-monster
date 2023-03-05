25e240c8add6178c47e0f3ccfd6b785b9269859e
Hannah Beattie <hannahdbeattie@gmail.com>
2023-03-05 15:10:26 +1300

    set up state variables for each phase of intro

--- app/intro.tsx
+++ app/intro.tsx
@@ -1,7 +1,8 @@
-import { Center, Heading } from 'native-base'
-import React from 'react'
+import { Center } from 'native-base'
+import React, { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { setIntroPlayed } from '~features/intro/introSlice'
 import MonsterVoice from '~features/styledComponents/MonsterVoice'
-import PageWrapper from '~features/styledComponents/PageWrapper'
 
 // fade in from black, center screen
 //'GRRR!', 'ROAR', 'GROWLLLLL!', 'I am the dreaded....'
@@ -37,6 +38,20 @@ import PageWrapper from '~features/styledComponents/PageWrapper'
 // MAYBE LATER --- '/' === intro played
 
 function intro() {
+	const dispatch = useDispatch()
+	const [state1, setState1] = useState(false)
+	const [state2, setState2] = useState(false)
+	const [state3, setState3] = useState(false)
+
+	useEffect(() => {
+		// Do something when any of the state values change
+		console.log('state1:', state1)
+		console.log('state2:', state2)
+		console.log('state3:', state3)
+	}, [state1, state2, state3])
+
+	const introPlayed = () => dispatch(setIntroPlayed(true))
+
 	return (
 		<Center flex={1} bg={'black'}>
 			<MonsterVoice sizeVal={'8xl'} color='blueGray.200'>
