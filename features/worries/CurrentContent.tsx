3b56ce92ae3c39174b52d88ca41bf89dbfed7183
Hannah Beattie <hannahdbeattie@gmail.com>
2023-03-08 21:38:06 +1300

    add delete worry slide to current worries

--- features/worries/CurrentContent.tsx
+++ features/worries/CurrentContent.tsx
@@ -1,17 +1,17 @@
+import { Entypo } from '@expo/vector-icons'
 import { useRouter } from 'expo-router'
-import { Center, Text, VStack } from 'native-base'
-import React from 'react'
+import { Center, Icon, IconButton, Text, VStack } from 'native-base'
+import React, { useState } from 'react'
 import { useWindowDimensions } from 'react-native'
 import { useDispatch, useSelector } from 'react-redux'
 import SwipeableButton from '~features/styledComponents/SwipableButton'
 import { useFormatDate } from '~features/worries/useFormatDate'
-import { Worry, selectAllActive, updateWorry } from '~features/worries/worrySlice'
+import { Worry, selectAllActive, updateWorry, deleteWorry } from '~features/worries/worrySlice'
 
 export default function CurrentContent() {
 	const allActive: Worry[] = [...useSelector(selectAllActive)].reverse()
 	const dispatch = useDispatch()
 	const router = useRouter()
-
 	const handleSwipe = (worryId: number) => {
 		dispatch(
 			updateWorry({
@@ -26,25 +26,28 @@ export default function CurrentContent() {
 		console.log('ate worry: ', worryId)
 	}
 
+	const handleTrash = (worryId: number) => {
+		dispatch(deleteWorry(worryId))
+		console.log('deleted worry: ', worryId)
+	}
+
 	const { width } = useWindowDimensions()
 	const w = width - 60
 	return (
 		<>
 			{allActive.map((worry) => (
-				<VStack key={worry.id} py={2}>
-					<SwipeableButton onSwipe={() => handleSwipe(worry.id)}>
+				<VStack py={2} key={worry.id}>
+					<SwipeableButton
+						onSwipe={() => handleSwipe(worry.id)}
+						onBin={() => handleTrash(worry.id)}
+					>
 						<VStack pt={3}>
 							<Center
-								backgroundColor={'#0e0e0e'}
 								justifyItems={'center'}
 								justifyContent={'center'}
-								borderColor={'#ffffff14'}
-								borderWidth={1}
-								py={8}
-								px={8}
-								width={w}
-								borderRadius={'md'}
 								alignItems={'stretch'}
+								py={6}
+								px={6}
 							>
 								<Text fontSize={'md'} color={'gray.400'}>
 									I started worrying {useFormatDate(worry.id)} {worry.description}
